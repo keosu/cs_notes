a2f6a08be586b9031ae19cb5d2875e5b6f5c7204
Yi Jianlong <jianlong.yi@xilinx.com>
2019-08-01 16:16:46 +0800

    update c++ template example: non-type template parameter
    Signed-off-by: Yi Jianlong <jianlong.yi@xilinx.com>

---
+++ code/c++/template.cpp
@@ -0,0 +1,65 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+//////////////////////////////////////////////////////////////////////
+//  non-type template parameter
+template <int size>
+class CTest {
+  int m_data[size];
+};
+
+CTest<10> obj;  // 将obj对象的成员 数值m_data大小初始为10
+//////////////////////////////////////////////////////////////////////
+
+//
+// c style
+// va_list va_start va_arg va_end
+//
+// call: printf("%f\n", SumOfFloat(3, 1.2f, 3.4, 5.6));  // 10.200000
+
+double SumOfFloat(int count, ...) {
+  va_list ap;
+  double sum = 0;
+  va_start(ap, count);  // 获得变长列表的句柄ap
+  for (int i = 0; i < count; i++)
+    sum += va_arg(ap, double);  // 每次获得一个参数
+  va_end(ap);
+  return sum;
+}
+/////////////////////////////////////////////////////////////////////////
+
+//
+// 变长模板
+//
+template <typename... Elements>
+class tuple;                                // 变长模板的声明
+template <typename Head, typename... Tail>  // 递归的偏特化定义
+class tuple<Head, Tail...> : private tuple<Tail...> {
+  Head head;
+};
+template <>
+class tuple<> {};  // 边界条件
+
+/////////////////////////////////////////////////////////////////////////
+
+//
+// 非参数模板
+//
+// Multiply<2, 3, 4, 5>::val
+// Multiply<2, 3, 4, 5>::val
+//
+template <long... nums>
+struct Multiply;
+
+template <long first, long... last>
+struct Multiply<first, last...> {
+  static const long val = first * Multiply<last...>::val;
+};
+template <>
+struct Multiply<> {
+  static const long val = 1;
+};
+
+int main() {
+  printf("%f\n", SumOfFloat(3, 1.2f, 3.4, 5.6));  // 10.200000
+}
